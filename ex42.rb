30399ba2d00d90305a281238f9ff101dcbe7c5cf
Hugh Morrison <hmorri32@gmail.com>
2017-06-25 13:36:40 -0600

    42

---
+++ ex42.rb
@@ -0,0 +1,48 @@
+class Animal
+end
+
+class Dog < Animal
+  def initialize(name)
+    @name = name
+  end
+end
+
+class Cat < Animal
+  def initialize(name)
+    @name = name
+  end
+end
+
+class Person
+  def initialize(name)
+    @name = name
+    @pet  = nil
+  end
+  attr_accessor :pet
+end
+
+class Employee < Person
+  def initialize(name, salary)
+    super(name)
+    @salary = salary
+  end
+end
+
+class Fish
+end
+
+class Salmon < Fish
+end
+
+class Halibut < Fish
+end
+
+rover     = Dog.new()
+satan     = Cat.new()
+mary      = Person.new()
+mary.pet  = satan
+frank     = Employee.new('frank', 1)
+frank.pet = rover
+flipper   = Fish.new()
+crouse    = Salmon.new()
+harry     = Halibut.new()
