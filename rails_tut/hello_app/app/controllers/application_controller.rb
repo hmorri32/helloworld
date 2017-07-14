aac68dafb82bd641361d20b1042d2363c3acfbf3
Hugh Morrison <hmorri32@gmail.com>
2017-07-14 12:06:20 -0400

    railstut routes

--- rails_tut/hello_app/app/controllers/application_controller.rb
+++ rails_tut/hello_app/app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: 'suh dude!'
+  end
+
+  def burritos
+    render html: 'dealwitit'
+  end
 end
