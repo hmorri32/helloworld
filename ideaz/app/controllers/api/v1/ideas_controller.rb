15b0a369e326adac85dd100d4abb051b0dcb1d91
Hugh Morrison <hmorri32@gmail.com>
2017-08-14 15:11:44 -0600

    remove ideabox

--- ideaz/app/controllers/api/v1/ideas_controller.rb
+++
@@ -1,7 +0,0 @@
-class Api::V1::IdeasController < ApplicationController
-  respond_to :json
-
-  def index
-    respond_with Idea.all
-  end
-end
--- ideaz/app/models/idea.rb
+++
@@ -1,5 +0,0 @@
-class Idea < ApplicationRecord
-  validates :title, :body, presence: true
-
-  enum quality: [:swill, :plausible, :genius]
-end
